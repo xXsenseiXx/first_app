501e70546f44498c78d3cb5bb555439e19758934
unknown <yasserraven@gmail.com>
2025-03-21 03:58:52 +0100

    add the movies imgs to front page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/movieCard";
 
 export default function Index() {
   const router = useRouter();
+
+  const { data: movies, loading: moviesLoading, error: moviesError} = useFetch(() => fetchMovies({
+    query:''
+  }))
   
   return (
     <View className="flex-1 bg-primary">
@@ -15,12 +22,43 @@ export default function Index() {
        contentContainerStyle={{minHeight:"100%", paddingBottom: 10}}>
         <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar 
-            onPress={() => router.push('/search')}
-            placeholder="Search for a movie"
-          /> 
-        </View>
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="self-center mt-10"/>
+        ) : moviesError ? (
+          <text>Error: {moviesError.message}</text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar 
+              onPress={() => router.push('/search')}
+              placeholder="Search for a movie"
+            /> 
+            <> 
+              <Text className="mt-5 mb-3 text-lg font-bold text-white ">Latest movie</Text>
+            
+             
+              <FlatList 
+                data={movies}
+                renderItem={({item}) => (
+                  <MovieCard 
+                    {...item}
+                  />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: 'flex-start',
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="pb-32 mt-2"
+                scrollEnabled={false} 
+              />
+            </>
+          </View>
+        )}
+
+        
       </ScrollView>
     </View>
   );
---
+++ components/movieCard.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'expo-router';
+import React from 'react';
+import { Text, StyleSheet, View, Touchable, TouchableOpacity, Image } from 'react-native';
+
+const MovieCard = ({ id, poster_path, title, vote_average, 
+    release_date} : Movie) => {
+
+    
+        
+    return (
+        <Link href={`/movies/${id}`} asChild> 
+            <TouchableOpacity className='w-[30%] '>
+                <Image 
+                    source={{uri: poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` 
+                    : 'https://via.placeholder.co/600x400/1a1a1a/ffffff.png'}}
+                    className='w-full rounded-lg h-52'
+                    resizeMode='cover'
+                />
+                <Text className='text-sm font-bold text-white'>{title}</Text>
+            </TouchableOpacity>
+        </Link>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default MovieCard;
